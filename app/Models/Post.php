b75e4cf9e85f741e77e3087d66f2c25b2b4f1780
al221910432 <90278124+al221910432@users.noreply.github.com>
2021-09-29 10:01:01 -0500

    Add files via upload

---
+++ app/Models/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+}
+
