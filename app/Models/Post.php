a213c67a27f77f4deb4406ce5c8d5e85c8084432
al221910432 <90278124+al221910432@users.noreply.github.com>
2021-10-18 10:40:21 -0500

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,57 +1,18 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class Post extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var string[]
+     * Get the comments for the blog post.
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
     
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-   
-    public function roles()
-    {
-        return $this->belongsToMany('App\Role');
-    }
 }
-
-
-
