00f6192d54424b22a0072b50f83e4ba0ac088589
al221910432 <90278124+al221910432@users.noreply.github.com>
2021-11-10 09:23:06 -0600

    Create ControllerArticle.php

---
+++ app/Http/Controllers/ControllerArticle.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Article;
+use Illuminate\Http\Request;
+
+class ControllerArticle extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $articles = Article::all();
+        return view('articles.index',[
+            'articles'=> $articles
+            ]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+        Article::create([
+        'title'=>$request->title,
+        'img'=>$request->img,
+        'subtitle'=>$request->subtitle,
+        'body'=>$request->body,
+        'category_id'=>$request->category_id,
+        'img_id'=>$request->img_id
+        ]);
+        return ('el article se dio de alta de manera correcta');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
